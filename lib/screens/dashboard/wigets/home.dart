c7b96bd165d24fa5b0a074d7ef75f1c00402301f
Ujjwal <ugirase2003@gmail.com>
2024-07-10 09:20:20 +0530

    minor chng

--- lib/screens/dashboard/wigets/home.dart
+++ lib/screens/dashboard/wigets/home.dart
@@ -26,99 +26,99 @@ class _HomeState extends State<Home> {
       padding: const EdgeInsets.all(20),
       child: FutureBuilder(
         future: AuthServices().getUserData(),
-        builder: (context, snapshot) =>
-            snapshot.connectionState == ConnectionState.waiting
-                ? const Center(
-                    child: CircularProgressIndicator(),
-                  )
-                : SafeArea(
-                    child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+        builder: (context, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : SafeArea(
+                child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
                     children: [
-                      Row(
+                      Text(
+                        "${snapshot.data!["username"]}",
+                        style: Theme.of(context).textTheme.headlineMedium,
+                      ),
+                      const Spacer(),
+                      ElevatedButton(
+                          onPressed: () {
+                            FirebaseAuth.instance
+                                .signOut()
+                                .whenComplete(() => Navigator.pushReplacement(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => SignIn(),
+                                    )));
+                          },
+                          child: const Text("Logout")),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text("Horizontal List"),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        roundedImg("assets/images/food1.jpg"),
+                        roundedImg("assets/images/food1.jpg"),
+                        roundedImg("assets/images/food1.jpg"),
+                        roundedImg("assets/images/food1.jpg"),
+                        roundedImg("assets/images/food1.jpg"),
+                        roundedImg("assets/images/food1.jpg"),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text("Vertical List"),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Card(
+                    color: Colors.blueGrey,
+                    child: Column(
+                      children: [
+                        roundedImg("assets/images/food1.jpg"),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        roundedImg("assets/images/food1.jpg"),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                        roundedImg("assets/images/food1.jpg"),
+                        const SizedBox(
+                          height: 5,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Text("Grid View"),
+                  Card(
+                    color: Colors.white24,
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      child: GridView(
+                        shrinkWrap: true,
+                        gridDelegate: _gridDelegateWithFixedCrossAxisCount,
                         children: [
-                          Text(
-                            "${snapshot.data!["username"]}",
-                            style: Theme.of(context).textTheme.headlineMedium,
-                          ),
-                          const Spacer(),
-                          ElevatedButton(
-                              onPressed: () {
-                                FirebaseAuth.instance
-                                    .signOut()
-                                    .whenComplete(() => Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => SignIn(),
-                                        )));
-                              },
-                              child: const Text("Logout")),
+                          roundedImg("assets/images/food1.jpg"),
+                          roundedImg("assets/images/food1.jpg"),
+                          roundedImg("assets/images/food1.jpg"),
                         ],
                       ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const Text("Horizontal List"),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            roundedImg("assets/images/food1.jpg"),
-                            roundedImg("assets/images/food1.jpg"),
-                            roundedImg("assets/images/food1.jpg"),
-                            roundedImg("assets/images/food1.jpg"),
-                            roundedImg("assets/images/food1.jpg"),
-                            roundedImg("assets/images/food1.jpg"),
-                          ],
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const Text("Vertical List"),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      Card(
-                        color: Colors.blueGrey,
-                        child: Column(
-                          children: [
-                            roundedImg("assets/images/food1.jpg"),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            roundedImg("assets/images/food1.jpg"),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                            roundedImg("assets/images/food1.jpg"),
-                            const SizedBox(
-                              height: 5,
-                            ),
-                          ],
-                        ),
-                      ),
-                      const Text("Grid View"),
-                      Card(
-                        color: Colors.white24,
-                        child: Container(
-                          padding: const EdgeInsets.all(10),
-                          child: GridView(
-                            shrinkWrap: true,
-                            gridDelegate: _gridDelegateWithFixedCrossAxisCount,
-                            children: [
-                              roundedImg("assets/images/food1.jpg"),
-                              roundedImg("assets/images/food1.jpg"),
-                              roundedImg("assets/images/food1.jpg"),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  )),
+                    ),
+                  )
+                ],
+              )),
       ),
     );
   }
